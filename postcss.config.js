14a426488f2462f915a348b2c6442784795b8ed4
Nelson Daza <nelson.daza@gmail.com>
2020-06-05 15:19:18 -0500

    adds postcss conf for browser compat
    test plan:
     - NA
    
    qa risk: low

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  plugins: {
+    autoprefixer: null,
+    'postcss-discard-comments': null,
+    'postcss-hexrgba': null,
+    'postcss-browser-reporter': null,
+    'postcss-reporter': null,
+  },
+}
