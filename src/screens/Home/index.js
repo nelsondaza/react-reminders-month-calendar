d9fa232373ef1676b62774c0f764e358e44a800e
Nelson Daza <nelson.daza@gmail.com>
2020-06-06 00:22:50 -0500

    use moment for current date format
    test plan:
     - regressions
    
    qa risk: low

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,12 @@
 
 import React from 'react'
 
+import moment from 'moment'
+
 import styles from './index.scss'
 
 class Home extends React.PureComponent {
   render() {
-    const date = new Date()
-
     return (
       <div>
         <div className={styles.body}>
@@ -14,11 +14,7 @@ class Home extends React.PureComponent {
         </div>
         <footer className={styles.footer}>
           <b>Current date: </b>
-          {[
-            date.getFullYear(),
-            `00${date.getMonth()}`.slice(-2),
-            `00${date.getDate()}`.slice(-2),
-          ].join('-')}
+          {moment().format('YYYY-MM-DD')}
         </footer>
       </div>
     )
