61e73290e0903a5dc535a1775780f5cf99a006d9
Nelson Daza <nelson.daza@gmail.com>
2020-06-06 16:02:15 -0500

    force event to have forecast as object
    test plan:
     - yarn test
    
    qa risk: low

--- src/schemas/EventSchema.js
+++ src/schemas/EventSchema.js
@@ -2,12 +2,13 @@
 import PropTypes from 'prop-types'
 
 import CitySchema from './CitySchema'
+import WeatherSchema from './WeatherSchema'
 
 export default PropTypes.shape({
   city: CitySchema.isRequired,
   color: PropTypes.string,
   datetime: PropTypes.number.isRequired,
   description: PropTypes.string.isRequired,
-  forecast: PropTypes.string,
+  forecast: WeatherSchema.isRequired,
   id: PropTypes.number.isRequired,
 })
