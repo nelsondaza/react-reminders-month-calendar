858e8d7120f0bb9cc1e0a856a12c38884984b6b7
Nelson Daza <nelson.daza@gmail.com>
2020-06-06 02:50:48 -0500

    render a list of Day in MonthCalendar depending on current month
    test plan:
     - yarn stories > MonthCalendar
    
    qa risk: low

--- src/components/MonthCalendar/index.js
+++ src/components/MonthCalendar/index.js
@@ -3,12 +3,40 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import classnames from 'classnames'
+import moment from 'moment'
+
+import Day from './Day'
 
 import styles from './index.scss'
 
 class MonthCalendar extends React.PureComponent {
+  renderDays() {
+    const today = moment().startOf('day')
+    const firstDay = today.clone()
+      .startOf('month').startOf('week')
+    const lastDay = today.clone()
+      .endOf('month').endOf('week')
+
+    const days = []
+    while(lastDay.diff(firstDay, 'days', true) >= 0) {
+      days.push(
+        <Day
+          className={styles.day}
+          day={firstDay.format('D')}
+          highlight={firstDay.format('d') === '0'}
+          key={firstDay.valueOf()}
+          readOnly={!firstDay.isSame(today, 'month')}
+        />
+      )
+      firstDay.add(1, 'days')
+    }
+
+    return days
+  }
+
   render() {
     const { className } = this.props
+
     return (
       <div className={classnames(className, styles.wrapper)}>
         <div className={styles.titles}>
@@ -20,41 +48,8 @@ class MonthCalendar extends React.PureComponent {
           <div>Friday</div>
           <div>Saturday</div>
         </div>
-        <div className={styles.days}>
-          <div>Sunday</div>
-          <div>Monday</div>
-          <div>Tuesday</div>
-          <div>Wednesday</div>
-          <div>Thursday</div>
-          <div>Friday</div>
-          <div>Saturday</div>
-        </div>
-        <div className={styles.days}>
-          <div>Sunday</div>
-          <div>Monday</div>
-          <div>Tuesday</div>
-          <div>Wednesday</div>
-          <div>Thursday</div>
-          <div>Friday</div>
-          <div>Saturday</div>
-        </div>
-        <div className={styles.days}>
-          <div>Sunday</div>
-          <div>Monday</div>
-          <div>Tuesday</div>
-          <div>Wednesday</div>
-          <div>Thursday</div>
-          <div>Friday</div>
-          <div>Saturday</div>
-        </div>
-        <div className={styles.days}>
-          <div>Sunday</div>
-          <div>Monday</div>
-          <div>Tuesday</div>
-          <div>Wednesday</div>
-          <div>Thursday</div>
-          <div>Friday</div>
-          <div>Saturday</div>
+        <div className={styles.body}>
+          {this.renderDays()}
         </div>
       </div>
     )
