680fb84997714f72ef36f16a3845e762948970ad
Nelson Daza <nelson.daza@gmail.com>
2020-06-06 00:19:33 -0500

    sets default route / to Home screen
    test plan:
     - yarn serve
     - nav to /
     - check content
    
    qa risk: low

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,15 @@ import { Route, Switch } from 'react-router-dom'
 import ProgressBar from 'components/ProgressBar'
 import Site from 'screens/Site'
 
+const HomePage = lazy(() => import(/* webpackChunkName: 'home' */ 'screens/Home'))
 const NotFoundPage = lazy(() => import(/* webpackChunkName: 'not_found' */ 'screens/NotFound'))
 
 export default () => (
   <Site>
     <Suspense fallback={<ProgressBar ariaLabel="Cargando..." value={100} />}>
       <Switch>
+        <Route path="/" component={HomePage} exact />
+
         <Route component={NotFoundPage} />
       </Switch>
     </Suspense>
