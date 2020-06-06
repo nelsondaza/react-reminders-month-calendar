d18d04de3ecb1cd219a696c511ca99172b327ac6
Nelson Daza <nelson.daza@gmail.com>
2020-06-05 20:33:50 -0500

    creates a 404 not found page screen
    test plan:
     - NA
    
    qa risk: low

---
+++ src/screens/NotFound/index.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+class NotFound extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Page not found</h1>
+        <p>404</p>
+      </div>
+    )
+  }
+}
+
+export default NotFound
