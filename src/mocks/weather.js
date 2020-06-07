912551ed583d35e5f944f9d69c9675827caabff4
Nelson Daza <nelson.daza@gmail.com>
2020-06-07 03:54:00 -0500

    lint files
    test plan:
     - NA
    
    qa risk: low

--- src/mocks/weather.js
+++ src/mocks/weather.js
@@ -2,8 +2,8 @@
 import buildMock from './buildMock'
 
 export default buildMock({
-  id: 20,
-  name: 'Clouds',
   description: 'broken clouds',
   icon: '04d',
+  id: 20,
+  name: 'Clouds',
 })
--- src/schemas/WeatherSchema.js
+++ src/schemas/WeatherSchema.js
@@ -2,8 +2,8 @@
 import PropTypes from 'prop-types'
 
 export default PropTypes.shape({
-  id: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired,
   description: PropTypes.string,
   icon: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
 })
